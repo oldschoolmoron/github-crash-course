5c15d0abc0ae61cd797d48af67727d914be60137
Ananya Pathak <anyalko043@gmail.com>
2024-05-29 23:33:54 +0530

    Intial code Added

---
+++ initial.js
@@ -0,0 +1,10 @@
+const age = 24;
+const name = 'Ananya Pathak'
+const dob = '03/04/1998'
+
+function sayName(){
+    console.log('My name is' + name)
+}
+function sayName2(){
+    console.log('My name is' + name)
+}
